2ca7b2f067add3702cdab684a1d0cf042e790474
v.petrenko <v.petrenko@nic.ru>
2025-04-15 03:54:20 +0300

    fix

--- backend/migrations/migrate.pl
+++ backend/migrations/migrate.pl
@@ -2,17 +2,22 @@
 use strict;
 use warnings FATAL => 'all';
 
+use JSON qw(decode_json);
+use lib 'migrations';
 use TableChecker;
 
 # Загрузка конфигураций
 my $config = decode_json do { local (@ARGV, $/) = ('config/config.json'); <> };
 
-# Подключение к базе данных
-my $db_config = "DBI:Pg:dbname=$config->{'database'}{'dbname'};
-           host=$config->{'database'}{'host'};
-           $config->{'database'}{'username'};
-           $config->{'database'}{'password'}";
+# Приводим к нужному виду
+my $db_config = {
+    host => $config->{'database'}{'host'},
+    port => 5432,
+    username => $config->{'database'}{'username'},
+    password => $config->{'database'}{'password'},
+    dbname => $config->{'database'}{'dbname'}
+};
 
-my $sql_files_dir = './sql/';
+my $sql_files_dir = './migrations/sql/';
 
 TableChecker::check_and_create_tables($db_config, $sql_files_dir);
